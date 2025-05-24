94c3542c3d312260449d60b4a76f800ea80b8bfa
3mptine2s <ebuckerer@hotmail.com>
2025-05-25 01:59:49 +0700

    makingusre

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -2,19 +2,13 @@
 #include <stdio.h>
 void ft_putnbr(int nbr)
 {
-    if (nbr > __INT_MAX__)
-    {
-    }
-    if (nbr < __WINT_MIN__)
-    {
-    }
-    
+    printf("%d",&nbr);
 }
 
-int main(void)
-{
-    int max = 2147483647;
-    int min = -2147483647;
-    printf("%d",max);
-    printf("%d",min);
-}
+// int main(void)
+// {
+//     int max = 2147483647;
+//     int min = -2147483647;
+//     printf("%d",max);
+//     printf("%d",min);
+// }
